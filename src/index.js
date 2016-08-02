7d5fd46c80d2a842b6e024c8a0c0153a226b94ee
Anuj Khare <khareanuj18@gmail.com>
2016-08-03 00:12:19 +0530

    remove parts of demo

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
-require('./favicon.ico'); // Tell webpack to load favicon.ico
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
